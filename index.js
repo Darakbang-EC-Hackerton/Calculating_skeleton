1303829cd93a40a49d671e65f50b4b895bf1dfa1
STkangyh <k22101149@g.seoultech.ac.kr>
2024-10-27 01:55:04 +0900

    Skeleton Code

---
+++ index.js
@@ -0,0 +1,16 @@
+function appendNumber(){
+    // To-Do: 숫자를 추가하는 코드를 만들어주세요!
+}
+
+function appendOperator(){
+    // To-Do: 연산자를 추가하는 코드를 만들어주세요!
+}
+
+function ClearDisplay(){
+    // To-Do: 계산 과정을 삭제하는 코드를 만들어주세요!
+}
+
+function calculateResult(){
+    // To-Do: 식을 계산하는 코드를 만들어주세요!
+    // 발생할 수 있는 error에 대한 적절한 처리를 해주세요!
+}
