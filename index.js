353980689ffba7e1020d44f787720202d1a9ad5a
STkangyh <k22101149@g.seoultech.ac.kr>
2024-10-28 13:32:34 +0900

    일부 Function Method Name 삭제

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ function appendOperator(){
     // To-Do: 연산자를 추가하는 코드를 만들어주세요!
 }
 
-function ClearDisplay(){
-    // To-Do: 계산 과정을 삭제하는 코드를 만들어주세요!
-}
+// To-Do: 계산 과정을 삭제하는 함수를 만들어주세요!
 
-function calculateResult(){
-    // To-Do: 식을 계산하는 코드를 만들어주세요!
-    // 발생할 수 있는 error에 대한 적절한 처리를 해주세요!
-}
+// To-Do: 식을 계산하는 코드를 만들어주세요!
+// 발생할 수 있는 error에 대한 적절한 처리를 해주세요!
